1fb9c9fb5e1f1195f92b7811f4e42455b2e2267d
qinghuanI <1062217965@qq.com>
2022-05-06 07:43:10 +0800

    support css modules

--- src/pages/page2/index.jsx
+++ src/pages/page2/index.jsx
@@ -1,10 +1,12 @@
+import styles from './index.module.scss';
+
 export default function Page2() {
   const handleClick = () => {
     throw Error('hello');
   };
 
   return (
-    <div>
+    <div className={styles.div}>
       this is Page2 component!
       <button onClick={handleClick}>test</button>
     </div>
